faa15d5c479cda505a3c3a601de96bbb23b2b340
specialistik <test04@list.ru>
2016-12-26 08:43:13 +0000

    phones

--- smart_spider/smart_spider/spiders/pages.py
+++ smart_spider/smart_spider/spiders/pages.py
@@ -6,6 +6,9 @@ class PagesSpider(scrapy.Spider):
     start_urls = [
         'http://homes.e1.ru/kupit/rooms-1,2,3,4,5/?gorod=all&form=11102&zhilaya_novostrojki=1',
     ]
+    
+    def get_phone(self, response):
+		phone_ajax_link = response.css('#show_phone').xpath('@data-link').extract_first()
 
     def get_rooms_links(self, response):
 		for quote in response.css('td.re-search-result-table__body-cell_price'):
@@ -25,7 +28,7 @@ class PagesSpider(scrapy.Spider):
 		self.rooms.create()
 
 		last_page = int(response.css('.re-pagination-side-link::text').extract()[-1])
-		for page in range(2, 5):  #last_page):
+		for page in range(2, last_page):  #last_page):
 		    yield scrapy.Request(base_url + str(page), callback=self.get_rooms_links)
 
 
